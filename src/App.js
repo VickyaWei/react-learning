f6cdb6b7c7421ced05011bb8b4697d3792937567
VickyaWei <vickyaa.wei@gmail.com>
2024-08-27 15:29:13 -0700

    First commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Accordion from "./components/accordion";
+
+function App() {
+  return (
+    <div>
+      {/* Accordion component */}
+      <Accordion />
+    </div>
+  );
+}
+
+export default App;
