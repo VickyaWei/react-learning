a51c295221ed31c4fd6789e77748b7ffbbd58763
VickyaWei <vickyaa.wei@gmail.com>
2024-08-27 16:52:40 -0700

    second commit

--- src/components/random-color/index.js
+++ src/components/random-color/index.js
@@ -8,7 +8,7 @@ export default function RandomColor() {
     return Math.floor(Math.random() * length);
   }
   function handleCreateHexColor() {
-    const hex = [1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+    const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
     let hexColor = "#";
     for (let i = 0; i < 6; i++) {
       hexColor += hex[handleRandom(hex.length)];
